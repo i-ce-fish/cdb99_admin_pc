4b24e05131a0c3160f78ec78bd0e9d2f19577a74
binyu <binyu>
2020-06-25 03:36:32 +0800

    init

---
+++ src/router/modules/goods.js
@@ -0,0 +1,30 @@
+
+import Layout from "@/layout"
+
+const goodsRouter = {
+  path: "/goods",
+  name: "goods",
+  redirect: "/goods/index",
+  component: Layout,
+  children: [{
+    path: "index",
+    name: "goods",
+    component: () => import("@/views/goods"),
+    meta: { title: "商品管理", icon: "tree" }
+  }, {
+    path: "edit",
+    component: () => import("@/views/goods/edit"),
+    hidden: true,
+    meta: { title: "修改商品" }
+  },
+  {
+    path: "add",
+    component: () => import("@/views/goods/add"),
+    hidden: true,
+    meta: { title: "添加商品" }
+  }
+  ]
+}
+
+export default goodsRouter
+
